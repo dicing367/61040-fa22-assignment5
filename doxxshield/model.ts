4a0db979dd3797080fbe9e8787726e40c9cfe6af
Mohammed Shafim <mshafim@mit.edu>
2022-10-25 17:48:26 -0400

    created model.ts for doxshield

---
+++ doxxshield/model.ts
@@ -0,0 +1,45 @@
+import type {Types} from 'mongoose';
+import {Schema, model} from 'mongoose';
+import type {Freet} from '../freet/model';
+import type {User} from '../user/model';
+
+/**
+ * This file defines the properties stored in DoxxShield
+ * DO NOT implement operations here ---> use collection file
+ */
+
+// Type definition for DoxxShield on the backend
+
+export type Rating = {
+  _id: Types.ObjectId; // MongoDB assigns each object this ID on creation
+  user: User;
+  freet: Freet;
+  warnings: Array<[string, string]>;
+};
+
+// Mongoose schema definition for interfacing with a MongoDB table
+// Users stored in this table will have these fields, with the
+// type given by the type property, inside MongoDB
+const RatingSchema = new Schema({
+  // The user
+  user: {
+    // Use Types.ObjectId outside of the schema
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  // The user's freet
+  freet: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Freet'
+  },
+  // a list of warnings for compromising info in the freet
+  warnings: {
+    type: Array<[string, string]>,
+    required: true
+  }
+});
+
+const RatingModel = model<Rating>('Rating', RatingSchema);
+export default RatingModel;
